1c71515f1b9ded074150a71395cda1bec9c4d574
morigutiyuusuke <dhrnuzny2@gmail.com>
2022-05-05 07:22:27 +0000

    comics

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
+  resources :comics, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+    
  
 end
---
+++ db/migrate/20220505071415_create_comics.rb
@@ -0,0 +1,15 @@
+class CreateComics < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comics do |t|
+      
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.string :author
+      t.string :genre
+      t.float :rate
+
+      t.timestamps
+    end
+  end
+end
